38ee0991e7f0619234525599c719cb07e6959dd3
BetterZhang <betterzhang.dev@gmail.com>
2017-06-28 13:20:49 +0800

    泛型Java、Kotlin对比实现

---
+++ src/com/betterzhang/learnkotlin/java/Wrapper.java
@@ -0,0 +1,27 @@
+package com.betterzhang.learnkotlin.java;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/28 下午 1:12
+ * Desc   : description
+ */
+public final class Wrapper<T> {
+
+    private T item;
+
+    public Wrapper(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public static void main(String[] args) {
+        Wrapper<String> wrapper = new Wrapper<>("Hello Java");
+        System.out.println(wrapper.getItem());
+    }
+
+}
---
+++ src/com/betterzhang/learnkotlin/kotlin/Wrapper.kt
@@ -0,0 +1,15 @@
+package com.betterzhang.learnkotlin.kotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/28 下午 1:18
+ * Desc   : description
+ */
+class Wrapper<T>(val item: T)
+
+fun main(args: Array<String>) {
+    var wrapper = Wrapper("Hello Kotlin")
+    println(wrapper.item)
+}
