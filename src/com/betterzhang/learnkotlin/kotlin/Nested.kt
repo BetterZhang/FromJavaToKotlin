920d7b317a168f2a382ff1cba0f481b3fa70beb5
BetterZhang <betterzhang.dev@gmail.com>
2017-06-28 14:07:51 +0800

    Kotlin嵌套函数

---
+++ src/com/betterzhang/learnkotlin/kotlin/Nested.kt
@@ -0,0 +1,16 @@
+package com.betterzhang.learnkotlin.kotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/28 下午 2:05
+ * Desc   : Kotlin嵌套函数
+ */
+
+fun main(args: Array<String>) {
+    fun sayHello() {
+        println("Hello Kotlin")
+    }
+    sayHello()
+}
