809721f69752256b229f4ab72b06bd1e84f708fd
BetterZhang <betterzhang.dev@gmail.com>
2017-06-28 11:06:41 +0800

    匿名内部类Kotlin对比实现

---
+++ src/com/betterzhang/learnkotlin/kotlin/View.kt
@@ -0,0 +1,32 @@
+package com.betterzhang.learnkotlin.kotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/28 上午 11:01
+ * Desc   : 匿名内部类
+ */
+
+interface OnClickListener {
+    fun onClick()
+}
+
+class View {
+
+    var listener: OnClickListener? = null
+
+    fun setOnClickListener(listener: OnClickListener) {
+        this.listener = listener
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val view = View()
+    view.setOnClickListener(object: OnClickListener {
+        override fun onClick() {
+            TODO("not implemented")
+        }
+    })
+}
